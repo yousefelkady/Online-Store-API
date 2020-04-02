95c459792c2b92f3749ec6a716e2acf5f762a6dd
Yousef Elkady <44307943+yousefelkady@users.noreply.github.com>
2020-04-02 08:48:32 -0700

    Final edit for database, listModel and controller classes

---
+++ database.php
@@ -0,0 +1,39 @@
+    <?php
+class Database{
+ 
+
+    private $host = "localhost";
+    private $db_name = "mystore";
+    private $username = "root";
+    private $password = "";
+    private static $instance = null ;
+    private $conn;
+ 
+
+    private function __construct(){
+        $this->conn = null;
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+
+    public static function getInstance()
+    {
+        if(!self::$instance)
+        {
+            self::$instance = new self();
+        }
+    
+        return self::$instance;
+    }
+    
+    public function getConnection(){
+        return $this->conn;
+    }
+}
+?>
